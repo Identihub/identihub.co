b0a325e20d4dcd056c2f454035a752dfc9148123
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-02-13 09:27:43 +0100

    Update jquery.cookiebar.js

--- js/jquery.cookiebar.js
+++ js/jquery.cookiebar.js
@@ -32,7 +32,7 @@
 			declineFunction: function(cookieValue){if(cookieValue=='enabled' || cookieValue=='accepted') window.location = window.location.href;}, //Function to run after decline
 			policyButton: true, //Set to true to show Privacy Policy button
 			policyText: 'Privacy Policy', //Text on Privacy Policy button
-			policyURL: '/privacy-policy/', //URL of Privacy Policy
+			policyURL: '/privacy', //URL of Privacy Policy
 			autoEnable: true, //Set to true for cookies to be accepted automatically. Banner still shows
 			acceptOnContinue: false, //Set to true to accept cookies when visitor moves to another page
 			acceptOnScroll: false, //Set to true to accept cookies when visitor scrolls X pixels up or down
