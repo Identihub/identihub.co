30c0e3926aa37a1170a0e874ec009b3448e9344e
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-02-13 09:19:14 +0100

    Update jquery.cookiebar.js

--- js/jquery.cookiebar.js
+++ js/jquery.cookiebar.js
@@ -44,7 +44,7 @@
 			element: 'body', //Element to append/prepend cookieBar to. Remember "." for class or "#" for id.
 			append: false, //Set to true for cookieBar HTML to be placed at base of website. Actual position may change according to CSS
 			fixed: false, //Set to true to add the class "fixed" to the cookie bar. Default CSS should fix the position
-			bottom: false, //Force CSS when fixed, so bar appears at bottom of website
+			bottom: true, //Force CSS when fixed, so bar appears at bottom of website
 			zindex: '', //Can be set in CSS, although some may prefer to set here
 			domain: String(window.location.hostname), //Location of privacy policy
 			referrer: String(document.referrer) //Where visitor has come from
